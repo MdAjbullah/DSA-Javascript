782af8bba9e6a1d0f258bb2bdeb259df04bc53ec
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2024-01-16 16:58:53 +0000

    code change

--- insertion.js
+++ insertion.js
@@ -1,15 +1,21 @@
 let data=[60,30,3,9,10,50];
-let newE1=90;
-let position=2;
-// console.log(data)
-for(let i= data.length-1; i>=0;i--){
-    // console.log(data[i])
-   if(i>=position){
-     data[i+1]=data[i];
-       if(i==position){
-        data[i]=newE1;
-       }
-    }
+// let newE1=90;
+// let position=2;
+// // console.log(data)
+// for(let i= data.length-1; i>=0;i--){
+//     // console.log(data[i])
+//    if(i>=position){
+//      data[i+1]=data[i];
+//        if(i==position){
+//         data[i]=newE1;
+//        }
+//     }
 
-}
+// }
+// console.log(data)
+let mydata=data.splice(3,0,500)// splice is used to direct insert to the any value in array
 console.log(data)
+
+
+
+
